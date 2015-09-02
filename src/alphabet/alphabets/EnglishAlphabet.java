fdbb7465135dc3eff74490a0c4746f12cd4218a7
Camilo Alaguna <camilourd@gmail.com>
2015-09-01 22:12:39 -0500

    geIndex function from EnglishAlphabet was optimized

--- src/alphabet/alphabets/EnglishAlphabet.java
+++ src/alphabet/alphabets/EnglishAlphabet.java
@@ -5,5 +5,10 @@ public class EnglishAlphabet extends SimpleAlphabet {
 	public EnglishAlphabet() {
 		super("abcdefghijklmnopqrstuvwxyz");
 	}
+	
+	@Override
+	public int getIndex(char car) {
+		return car - 'a';
+	}
 
 }
